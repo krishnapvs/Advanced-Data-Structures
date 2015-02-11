22c5ca17f923eda2397b2343c0dd0cece319ca47
Krishna VS Puvvada <vpuvvad@ncsu.edu>
2015-02-11 08:58:55 -0500

    Update assign.cpp

--- assign.cpp
+++ assign.cpp
@@ -1,20 +1,60 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <iostream>
+#include <sys/time.h>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-char *buf; /* Buffer to hold student record */ 
-FILE *fp; /* Input/output stream */ 
-long rec_off; /* Record offset */ 
-int rec_siz; /* Record size, in characters */ 
-/* If student.db doesn't exist, create it, otherwise read * its first record */ 
-if ( ( fp = fopen( "student.db", "r+b" ) ) == NULL ) 
-	{ 
-		fp = fopen( "student.db", "w+b" ); 
-	} 
-else 
-	{ 
-		rec_off = 0; 
-		fseek( fp, rec_off, SEEK_SET ); 
-		fread( &rec_siz, sizeof( int ), 1, fp ); 
-		buf = malloc( rec_siz + 1 ); 
-		fread( buf, 1, rec_siz, fp ); 
-		buf[ rec_siz ] = '\0'; 
+// struture to hold index 
+typedef struct 
+{ 
+	int key; /* Record's key */ 
+	long off; /* Record's offset in file */ 
+} index_S; 
+vector<index_S> prim; /* Primary key index */ 
+
+// structure to hold availability list
+typedef struct 
+{ 
+	int size; /* Hole's size */ 
+	long off; /* Hole's offset in file */ 
+} avail_S;
+vector<avail_S> avail_list; /* availability list containing the size */ 
+
+main()
+{
+	index_S dummy;
+	avail_S dummy2;
+	FILE *out; /* Output file stream */ 
+	if (( out = fopen( "index", "r+b" ) ) == NULL)
+	{
+		out = fopen( "index", "w+b" );
 	}
+	else
+	{
+		while(!feof(out))
+	 	{
+	 		fread(&dummy,sizeof(index_S),1,out);
+	 		prim.push_back(dummy);
+	 	}
+		 
+	}
+	fclose(out);
+	// Reading the input from availability list to the vector
+	if (( out = fopen( "availabilitylist", "r+b" ) ) == NULL)
+	{
+		out = fopen( "availabilitylist", "w+b" );
+	}
+	else
+	{
+		while(!feof(out))
+	 	{
+	 		fread(&dummy2,sizeof(avail_S),1,out);
+	 		avail_list.push_back(dummy2);
+	 	}
+		 
+	}
+	fclose(out);
+	
+}
